17729104d727abad652b6ce1cb784e23ef47a964
Jorgetano <143852381+Jorgetano@users.noreply.github.com>
2024-06-24 16:45:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,11 @@ if st.button("Script"):
 
 # Mostrar u ocultar el texto según el estado
 if st.session_state.show_text:
-    st.write("Colocar aquí tu lógica para desplazarte al inicio de la página.")
+    st.write("Don / Sra.…. de conformidad a lo dispuesto en la Ley 20.009, le informo que, para hacer efectiva su reclamación sobre aquellas operaciones respecto de las cuales desconoce haber otorgado su autorización o consentimiento, se solicita completar en su totalidad la Declaración Jurada que le enviaremos al correo electrónico que tiene registrado en nuestros sistemas y enviarla junto a su Cédula de Identidad, en un plazo de dos días
+
+Le sugerimos consultar con sus familiares o adicional si alguno realizó estas transacciones que usted está desconociendo.
+
+Finalmente es necesario indicarle, que se realiza una investigación interna de los desconocimiento realizados por los clientes, y en la medida que se cuente con evidencias de mecanismos robustos de seguridad, la empresa podrá hacer uso de las acciones legales que otorga la ley.")
 # Ingresar datos personales
 st.header("I. Antecedentes personales")
 nombre = st.text_input("Nombre Tarjetahabiente (Cardholder name)")
